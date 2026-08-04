ff6b12c8c716788ecb172be9f9adfc9368bb03b3
Jose Luis Chacón Cordón <joseluischaconcordon001@gmail.com>
2026-08-04 22:20:24 +0200

    cambios color fondo movil

--- css/js/contemplacion.js
+++ css/js/contemplacion.js
@@ -1,15 +1,29 @@
-document.body.style.background = "red";
 const imagenes = [
 
     "../images/hamadriades/hamadriades-01.jpg",
     "../images/hamadriades/hamadriades-02.jpg",
-    "../images/hamadriades/hamadriades-03.jpg",
-    "../images/hamadriades/hamadriades-04.jpg"
+    "../images/hamadriades/hamadriades-03.jpg"
 
 ];
 
 const foto = document.getElementById("foto-activa");
 
+const siguiente = document.querySelector(".next");
+
 let actual = 0;
 
 foto.src = imagenes[actual];
+
+siguiente.addEventListener("click", function(){
+
+    actual++;
+
+    if(actual >= imagenes.length){
+
+        actual = 0;
+
+    }
+
+    foto.src = imagenes[actual];
+
+});
